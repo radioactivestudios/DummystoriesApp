8fb0dc90017a45b9cb300ac703d09108896e2008
radioactive3 <akshay.csen@gmail.com>
2017-04-15 15:41:33 +0530

    First Commit

---
+++ app/src/main/java/com/radioactivestudios/hindihorrorstories/Home.java
@@ -0,0 +1,80 @@
+package com.radioactivestudios.hindihorrorstories;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class Home extends Activity {
+
+    ListView StoryList;
+    String[] Stories;
+    String SelectedCataegory;
+    String SelectedCatategory_id;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Intent intent = getIntent();
+        SelectedCataegory = intent.getStringExtra("selected-cataegory");
+         StoryList = (ListView) findViewById(R.id.HorrorstoryList);
+        switch (SelectedCataegory){
+            case "डायन प्रेत":
+                Stories = (String[]) getResources().getStringArray(R.array.Story_title_demon);
+                SelectedCatategory_id ="0";
+                break;
+            case "मुर्दे का प्यार":
+                Stories = (String[]) getResources().getStringArray(R.array.Story_title_deadlover);
+                SelectedCatategory_id="1";
+                break;
+            case "आत्मा का साया":
+                Stories= (String[])getResources().getStringArray(R.array.Story_title_soulShadow);
+                SelectedCatategory_id="2";
+                break;
+            case "शापित हवेली":
+                Stories= (String[])getResources().getStringArray(R.array.Story_title_hauntedHouse);
+                SelectedCatategory_id="3";
+                                break;
+            case "शमशान घाट":
+                Stories =(String[])getResources().getStringArray(R.array.Story_title_graveyard);
+                SelectedCatategory_id="4";
+                break;
+            default:
+                 Stories = (String[]) getResources().getStringArray(R.array.Story_title_deadlyToy);
+                SelectedCatategory_id= "5";
+
+        }
+
+        //String[] Stories = (String[]) getResources().getStringArray(R.array.Story_title);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_shortstories,
+                R.id.horrorStoryHomeText, Stories);
+        StoryList.setAdapter(adapter);
+
+        StoryList.setOnItemClickListener(new ListClickHandler());
+    }
+
+    public class ListClickHandler implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> adapter, View view, int position, long arg3) {
+            ArrayList<String> storyandandcategory = new ArrayList<>();
+            storyandandcategory.add(SelectedCatategory_id);
+            // TODO Auto-generated method stub
+            TextView listText = (TextView) view.findViewById(R.id.horrorStoryHomeText);
+            String text = listText.getText().toString();
+            storyandandcategory.add(text);
+            Intent intent = new Intent(Home.this, HorrorStoryActivity.class);
+            intent.putStringArrayListExtra("selected-item", storyandandcategory);
+            startActivity(intent);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/radioactivestudios/hindihorrorstories/HorrorStoryActivity.java
@@ -0,0 +1,79 @@
+package com.radioactivestudios.hindihorrorstories;
+
+/**
+ * Created by radioactive on 4/11/2017.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class HorrorStoryActivity extends Activity {
+TextView HorrorStoryText;
+    @Override
+    protected void onCreate(Bundle savedInstances){
+         String[] StoryTitles;
+          //(String[]) getResources().getStringArray(R.array.Story_title_demon);
+         String[] Full_story ;
+         /*(String[]) getResources().getStringArray(R.array.Full_Story);
+        HashMap<String,String> myMap = new HashMap<String,String>();
+        for (int i = 0; i < StoryTitles.length; i++) {
+            myMap.put(StoryTitles[i],Full_story[i]);
+        }*/
+
+        super.onCreate(savedInstances);
+        setContentView(R.layout.activity_horrorstory_1);
+
+        HorrorStoryText =(TextView) findViewById(R.id.HorrorStoryMainText);
+        //HorrorStoryText.setMovementMethod();
+        Intent intent = getIntent();
+        ArrayList<String> storyandcategory =intent.getStringArrayListExtra("selected-item");
+        String cataegory = storyandcategory.get(0);
+        String StoryTitle =storyandcategory.get(1);
+        switch (cataegory){
+            case "0":
+                StoryTitles = (String[]) getResources().getStringArray(R.array.Story_title_demon);
+                Full_story = (String[]) getResources().getStringArray(R.array.Story_demon_full);
+                break;
+            case "1":
+                StoryTitles = (String[]) getResources().getStringArray(R.array.Story_title_deadlover);
+                Full_story = (String[]) getResources().getStringArray(R.array.Stroy_full_dealdlover);
+                break;
+            case "2":
+                StoryTitles = (String[]) getResources().getStringArray(R.array.Story_title_soulShadow);
+               Full_story = (String[]) getResources().getStringArray(R.array.Story_SoulShadow_full);
+                break;
+            case "3":
+                StoryTitles = (String[]) getResources().getStringArray(R.array.Story_title_hauntedHouse);
+               Full_story = (String[]) getResources().getStringArray(R.array.Story_HauntedHouse_full);
+                break;
+            case "4":
+                StoryTitles = (String[]) getResources().getStringArray(R.array.Story_title_graveyard);
+                 Full_story = (String[]) getResources().getStringArray(R.array.Story_graveyard_full);
+                break;
+            default:
+                StoryTitles = (String[]) getResources().getStringArray(R.array.Story_title_deadlyToy);
+               Full_story = (String[]) getResources().getStringArray(R.array.Story_deadtlyToy_full);
+                break;
+        }
+
+        HashMap<String,String> myMap = new HashMap<String,String>();
+        for (int i = 0; i < StoryTitles.length; i++) {
+            myMap.put(StoryTitles[i],Full_story[i]);
+        }
+
+        String SelectedStory = myMap.get(StoryTitle);
+        setTitle(SelectedStory);
+        Toast.makeText(HorrorStoryActivity.this, "GridView Item: " +cataegory, Toast.LENGTH_LONG).show();
+        HorrorStoryText.setText(SelectedStory);
+        HorrorStoryText.setMovementMethod(new ScrollingMovementMethod());
+
+    }
+
+}
